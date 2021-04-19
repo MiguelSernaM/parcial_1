885e1fd3f89047605e6a04a76942b823351d84d5
MiguelSernaM <mangel.serna@udea.edu.co>
2021-04-19 15:33:21 -0500

    implementamos menu de usuario y funcion de verificacion

--- Parcial1_Codigo_Arduino.ino
+++ Parcial1_Codigo_Arduino.ino
@@ -1,86 +1,145 @@
 #define SER 2
 #define SRCLK 3
 #define RCLK 4
+//Funcion para prender todos los leds
 void verificacion();
+//Funcion para mostrar el caracter en los leds
 void imagen();
-byte ALL[] = {B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111};
-byte A[] = {B00000000,B00111100,B01100110,B01100110,B01111110,B01100110,B01100110,B01100110};
-byte B[] = {B01111000,B01001000,B01001000,B01110000,B01001000,B01000100,B01000100,B01111100};
-byte C[] = {B00000000,B00011110,B00100000,B01000000,B01000000,B01000000,B00100000,B00011110};
-byte D[] = {B00000000,B00111000,B00100100,B00100010,B00100010,B00100100,B00111000,B00000000};
-byte E[] = {B00000000,B00111100,B00100000,B00111000,B00100000,B00100000,B00111100,B00000000};
-byte F[] = {B00000000,B00111100,B00100000,B00111000,B00100000,B00100000,B00100000,B00000000};
-byte G[] = {B00000000,B00111110,B00100000,B00100000,B00101110,B00100010,B00111110,B00000000};
-byte H[] = {B00000000,B00100100,B00100100,B00111100,B00100100,B00100100,B00100100,B00000000};
-byte I[] = {B00000000,B00111000,B00010000,B00010000,B00010000,B00010000,B00111000,B00000000};
-byte J[] = {B00000000,B00011100,B00001000,B00001000,B00001000,B00101000,B00111000,B00000000};
-byte K[] = {B00000000,B00100100,B00101000,B00110000,B00101000,B00100100,B00100100,B00000000};
-byte L[] = {B00000000,B00100000,B00100000,B00100000,B00100000,B00100000,B00111100,B00000000};
-byte M[] = {B00000000,B00000000,B01000100,B10101010,B10010010,B10000010,B10000010,B00000000};
-byte N[] = {B00000000,B00100010,B00110010,B00101010,B00100110,B00100010,B00000000,B00000000};
-byte O[] = {B00000000,B00111100,B01000010,B01000010,B01000010,B01000010,B00111100,B00000000};
-byte P[] = {B00000000,B00111000,B00100100,B00100100,B00111000,B00100000,B00100000,B00000000};
-byte Q[] = {B00000000,B00111100,B01000010,B01000010,B01000010,B01000110,B00111110,B00000001};
-byte R[] = {B00000000,B00111000,B00100100,B00100100,B00111000,B00100100,B00100100,B00000000};
-byte S[] = {B00000000,B00111100,B00100000,B00111100,B00000100,B00000100,B00111100,B00000000};
-byte T[] = {B00000000,B01111100,B00010000,B00010000,B00010000,B00010000,B00010000,B00000000};
-byte U[] = {B00000000,B01000010,B01000010,B01000010,B01000010,B00100100,B00011000,B00000000};
-byte V[] = {B00000000,B00100010,B00100010,B00100010,B00010100,B00010100,B00001000,B00000000};
-byte W[] = {B00000000,B10000010,B10010010,B01010100,B01010100,B00101000,B00000000,B00000000};
-byte X[] = {B00000000,B01000010,B00100100,B00011000,B00011000,B00100100,B01000010,B00000000};
-byte Y[] = {B00000000,B01000100,B00101000,B00010000,B00010000,B00010000,B00010000,B00000000};
-byte Z[] = {B00000000,B00111100,B00000100,B00001000,B00010000,B00100000,B00111100,B00000000};
+//Funcion para leer 8 bites de cada fila y mirar si estan disponibles
+void desplazarbyte(uint8_t Pindato, uint8_t Pinreloj, uint8_t val);
+short int opcion;
+char carat, letra;
+int asciiletra;
+char abc[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
+int ALL[] = {255,255,255,255,255,255,255,255};
+int A[]  = {0,60,102,102,126,102,102,102};
+int B[]  = {120,72,72,112,72,68,68,124};
+int C[]  = {0,30,32,64,64,64,32,30};
+int D[]  = {0,56,36,34,34,36,56,0};
+int E[]  = {0,60,32,56,32,32,60,0};
+int F[]  = {0,60,32,56,32,32,32,0};
+int G[]  = {0,62,32,32,46,34,62,0};
+int H[]  = {0,36,36,60,36,36,36,0};
+int I[]  = {0,56,16,16,16,16,56,0};
+int J[]  = {0,28,8,8,8,40,56,0};
+int K[]  = {0,36,40,48,40,36,36,0};
+int L[]  = {0,32,32,32,32,32,60,0};
+int M[]  = {0,0,68,170,146,130,130,0};
+int N[]  = {0,34,50,42,38,34,0,0};
+int O[]  = {0,60,66,66,66,66,60,0};
+int P[]  = {0,56,36,36,56,32,3,0};
+int Q[]  = {0,60,66,66,66,70,62,1};
+int R[]  = {0,56,36,36,56,36,36,0};
+int S[]  = {0,60,32,60,4,4,60,0};
+int T[]  = {0,124,16,16,16,16,16,0};
+int U[]  = {0,66,66,66,66,36,24,0};
+int V[]  = {0,34,34,34,20,20,8,0};
+int W[]  = {0,130,146,84,84,40,0,0};
+int X[]  = {0,66,36,24,24,36,66,0};
+int Y[]  = {0,68,40,16,16,16,16,0};
+int Z[]  = {0,60,4,8,16,32,60,0};
+int uno[]  = {3,7,11,3,3,3,3,3};
+int dos[]  = {62,66,68,8,16,32,64,255};
+int tres[]  = {60,66,2,30,2,66,60,0};
+int cuatro[]  = {6,00010010,00100010,01000010,255,01000010,00000010,00000010,};
+int cinco[]  = {255,10100100,10100100,255,00000011,00000011,255,00000000};
+int seis[]  = {00001000,00010010,00100010,01111100,01000010,01000010,00111100,00000000};
+int siete[]  = {255,00000011,00000110,00001100,00011000,00110000,01100000,11000000};
+int ocho[]  = {00111100,01000010,01000010,00111100,01000010,01000010,00111100,00000000};
+int nueve[]  = {255,11000011,11000011,255,00000011,00000011,00000011,00000011};
+int cero[]  = {255,10000001,10000001,10000001,10000001,10000001,10000001,255};
+int cara1[] = {0,00100100,00100100,00000000,01000010,01000010,00111100,0};
+int cara2[] = {165,165,165,11100111,00000000,10000001,01011010,00100100};
+int cara3[] = {102,102,0,01111110,01000010,01000010,01111110,00000000};
+
 
 void setup(){
+  //inicializamos el puerto serial
+  Serial.begin(9600);
+  //pin de salida
   pinMode(SER, OUTPUT);
+  //pin de salida
   pinMode(SRCLK, OUTPUT);
+  //pin de salida
   pinMode(RCLK, OUTPUT);
+  Serial.println("Bienvend@. ¿Que desea hacer? ");
+  Serial.println("(1) Verificar que los leds funcionen");
+  Serial.println("(2) Mostrar un patron ingresado en los leds");
+  Serial.println("(3) Mostrar una secuencia de patrones en los leds");
+  Serial.println("(4) Salir ");
 }
 
 
 void loop(){
- verificacion();
+  //eleccion de usuario
   
-}
+  //cuando se ingresa un dato se cumple la condicion
+  if(Serial.available()){
+    //ciclo en donde se recibe la opcion del usuario
+    do{
+      opcion = Serial.read();
+      delay(200);
+      opcion = opcion-48;
+      //Opcion de verificacion
+      if(opcion == 1){
+      	verificacion();
+        opcion = 0;
+      }
+      //Opcion de mostrar patron
+      else if(opcion == 2){
+        Serial.println("Letra para imprimir: ");
+        letra = Serial.read();
+        delay(500);
+        asciiletra=letra;
+        if(asciiletra>64 && asciiletra<91 || asciiletra>96 && asciiletra <123)
+          
+        for(int i=0;i<26;i++){
+          if(letra==abc[i]){
+          	for (int i=0; i<8; i++) {
+            	//carat=abc[i];
+            	desplazarbyte(SER,SRCLK,~A[i]);
+                desplazarbyte(SER,SRCLK,128 >> i);  
+              
+          	}
+        }
+      
+      }
+      }
+      //Mostrar secuencia de patrones
+      else if(opcion == 3){
+        
+      }
+    }while(opcion <1 && opcion >3);
+  }
+}  
 void verificacion(){
-  for (int i=0; i<8; i++) {
-    shiftOut(SER, SRCLK, LSBFIRST, ~ALL[i]);
-    shiftOut(SER, SRCLK, LSBFIRST, 128 >> i);
-    /*
-    for(i=0; i<8; i++){
-      digitalWrite(SER,!!(~pic[i]&(1<<i)));
-      digitalWrite(SHIFT,1); 
-      digitalWrite(SHIFT,0);             
-    }
-       for(i=0; i<8; i++){
-      digitalWrite(SER,!!(128 >> i&(1<<i))); 
-      digitalWrite(SHIFT,1); 
-      digitalWrite(SHIFT,0);
-	}
-    */
+  for (int i=0; i<=8; i++) {
+
+    desplazarbyte(SER,SRCLK,~ALL[i]);
+    desplazarbyte(SER,SRCLK,128 >> i); 
+
   digitalWrite(RCLK, 1);
   digitalWrite(RCLK, 0);
-
   }
 }
-void imagen(){
-   for (int i=0; i<8; i++) {
-    shiftOut(SER, SRCLK, LSBFIRST, ~A[i]);
-    shiftOut(SER, SRCLK, LSBFIRST, 128 >> i);
-    /*
-    for(i=0; i<8; i++){
-      digitalWrite(SER,!!(~pic[i]&(1<<i)));
-      digitalWrite(SHIFT,1); 
-      digitalWrite(SHIFT,0);             
-    }
-       for(i=0; i<8; i++){
-      digitalWrite(SER,!!(128 >> i&(1<<i))); 
-      digitalWrite(SHIFT,1); 
-      digitalWrite(SHIFT,0);
+/*void imagen(){
+  Serial.println("Por favor, ingrese la letra que sea ver en pantalla: ");
+  Serial.read(letra);
+    for (int i=0; i<8; i++){
+   	1     >> i&(1<<i); 
+          digitalWrite(SHIFT,1);
+     
+}digitalWrite(SHIFT,0);
 	}
-    */
   digitalWrite(RCLK, 1);
   digitalWrite(RCLK, 0);
 
-  }
-
+*/ 
+void desplazarbyte(uint8_t Pindato, uint8_t Pinreloj, uint8_t val){
+  	uint8_t i; 
+    for (i = 0; i < 8; i++)  {
+        digitalWrite(Pindato, !!(val & (1 << i)));   
+            digitalWrite(Pinreloj, HIGH);
+            digitalWrite(Pinreloj, LOW);            
+      }
+  }    
