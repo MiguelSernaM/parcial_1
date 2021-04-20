25092a68bfd74c5344e97bae18b9eba7f5e250a9
Daniel <daniel.perez19@udea.edu.co>
2021-04-19 21:10:25 -0500

    Implementacion de la funcion Publik :D

--- Parcial1_Codigo_Arduino.ino
+++ Parcial1_Codigo_Arduino.ino
@@ -3,15 +3,15 @@
 #define RCLK 4
 //Funcion para prender todos los leds
 void verificacion();
-int **matrizdatos;
 //Funcion para mostrar el caracter en los leds
-void imagen(char);
+void imagen(char,int);
+//Funcion para mostrar los patrones
+void publik(char);
 //Funcion para leer 8 bites de cada fila y mirar si estan disponibles
 void desplazarbyte(uint8_t Pindato, uint8_t Pinreloj, uint8_t val);
 short int opcion;
-char carat, letra;
-int asciiletra;
-void mostrarleds(int L[]);
+char  letra;
+void mostrarleds(int L[],int);
 int A[]  = {0,60,102,102,126,102,102,102};
 int B[]  = {120,72,72,112,72,68,68,124};
 int C[]  = {0,30,32,64,64,64,32,30};
@@ -48,11 +48,7 @@ int siete[] = {255,3,6,12,24,48,96,192};
 int ocho[]  = {60,66,66,60,66,66,60,0};
 int nueve[] = {255,195,195,255,3,3,3,3};
 int cero[]  = {255,129,129,129,129,129,129,255};
-
-
-char abc[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
 int ALL[] = {255,255,255,255,255,255,255,255};
-
 int cara1[] = {0,36,36,0,66,66,60,0};
 int cara2[] = {165,165,165,231,0,129,90,36};
 int cara3[] = {102,102,0,126,66,66,126,0};
@@ -61,12 +57,7 @@ int cara3[] = {102,102,0,126,66,66,126,0};
 void setup(){
 //inicializamos el puerto serial
   Serial.begin(9600);
-  //llenamos la matriz
-  matrizdatos = new int *[36];
-   for(int i=0; i < 36; i++){
-        matrizdatos[i] = new int[8];
-   }
-  llenar_matriz(matrizdatos);
+
   //------------------------------
 
   //pin de salida
@@ -91,7 +82,6 @@ void loop(){
     //ciclo en donde se recibe la opcion del usuario
     
       opcion = Serial.read();
-      delay(200);
       opcion = opcion-48;
       //Opcion de verificacion
       if(opcion == 1){
@@ -100,18 +90,20 @@ void loop(){
       }
       //Opcion de mostrar patron
       else if(opcion == 2){
+        Serial.println("Por favor, ingrese la letra que sea ver en pantalla: ");
         while(true){
       if(Serial.available()){
-  	  	letra=Serial.read();      
-		imagen(letra);
+  	  	letra=Serial.read();   
+        delay(1000);
+		imagen(letra,5000);
         break;
       }
         }
         opcion = 0;
       }
       //Mostrar secuencia de patrones
-      else if(opcion == 3){
-        
+      else if(opcion == 3){ 
+        publik();
       }
     }while(opcion <1 && opcion >3);
   
@@ -121,13 +113,11 @@ void verificacion(){
 
     desplazarbyte(SER,SRCLK,~ALL[i]);
     desplazarbyte(SER,SRCLK,128 >> i); 
-
   digitalWrite(RCLK, 1);
   digitalWrite(RCLK, 0);
   }
 }
-void imagen(char letra){
-  Serial.println("Por favor, ingrese la letra que sea ver en pantalla: ");
+void imagen(char letra,int tiempo){
   
   delay(500);
     if(letra >= 97 && letra <= 122){
@@ -135,150 +125,160 @@ void imagen(char letra){
     }
       //para la letra A
       if(letra == 'A'){
-	  mostrarleds(A);
+	  mostrarleds(A,tiempo);
       }
       //para letra B
       else if(letra == 'B'){
-      mostrarleds(B);
+      mostrarleds(B,tiempo);
       }
       //para letra C
       else if(letra == 'C'){
-      	mostrarleds(C);
+      	mostrarleds(C,tiempo);
       }
       //para letra D
       else if(letra == 'D'){
-     	mostrarleds(D);
+     	mostrarleds(D,tiempo);
       }
       //para letra E
       else if(letra == 'E'){
-     mostrarleds(E);
+     mostrarleds(E,tiempo);
       }
       //para letra F
       else if(letra == 'F'){
-     	mostrarleds(F);
+     	mostrarleds(F,tiempo);
       }
       //para letra G
       else if(letra == 'G'){
-     	mostrarleds(G);
+     	mostrarleds(G,tiempo);
       }
       //para letra H
       else if(letra == 'H'){
-     	mostrarleds(H);
+     	mostrarleds(H,tiempo);
       }
       //para letra I
       else if(letra == 'I'){
-      	mostrarleds(I) ;
+      	mostrarleds(I,tiempo) ;
       }
       //para letra J
       else if(letra == 'J'){
-      	mostrarleds(J);
+      	mostrarleds(J,tiempo);
       }
       //para letra K
       else if(letra == 'K'){
-     	mostrarleds(K);
+     	mostrarleds(K,tiempo);
       }
       //para letra L
       else if(letra == 'L'){
-     	mostrarleds(L);
+     	mostrarleds(L,tiempo);
       }
       //para letra M
       else if(letra == 'M'){
-          mostrarleds(M);
+          mostrarleds(M,tiempo);
       }
       //para letra N
       else if(letra == 'N'){
-      	mostrarleds(N);
+      	mostrarleds(N,tiempo);
       }
       //para letra O
       else if(letra == 'O'){
-      	mostrarleds(O);
+      	mostrarleds(O,tiempo);
       }
       //para letra P
       else if(letra == 'P'){
-      	mostrarleds(P);
+      	mostrarleds(P,tiempo);
       }
       //para letra Q
       else if(letra == 'Q'){
-      	mostrarleds(Q);
+      	mostrarleds(Q,tiempo);
       }
       //para letra R
       else if(letra == 'R'){
-      	mostrarleds(R);
+      	mostrarleds(R,tiempo);
       }
       //para letra S
       else if(letra == 'S'){
-      	mostrarleds(S);
+      	mostrarleds(S,tiempo);
       }
       //para letra T
       else if(letra == 'T'){
-      	mostrarleds(T);
+      	mostrarleds(T,tiempo);
       }
       //para letra U
       else if(letra == 'U'){
-      	mostrarleds(U);
+      	mostrarleds(U,tiempo);
       }
       //para letra V
       else if(letra == 'V'){
-      	mostrarleds(V);
+      	mostrarleds(V,tiempo);
       }
       //para letra W
       else if(letra == 'W'){
-      	mostrarleds(W);
+      	mostrarleds(W,tiempo);
       }
       //para letra X
       else if(letra == 'X'){
-        mostrarleds(X);
+        mostrarleds(X,tiempo);
       }
       //para letra Y
       else if(letra == 'Y'){
-      mostrarleds(Y);
+      mostrarleds(Y,tiempo);
       }
       //para letra Z
       else if(letra == 'Z'){
-      	mostrarleds(Z);
+      	mostrarleds(Z,tiempo);
       }
       //para numero 0
       else if(letra == '0'){
-      	mostrarleds(cero);
+      	mostrarleds(cero,tiempo);
       }
       //para numero 1
       else if(letra == '1'){
-      	mostrarleds(uno);
+      	mostrarleds(uno,tiempo);
       }
       //para numero 2
       else if(letra == '2'){
-      	mostrarleds(dos);
+      	mostrarleds(dos,tiempo);
       }
       //para numero 3
       else if(letra == '3'){
-      	mostrarleds(tres);
+      	mostrarleds(tres,tiempo);
       }
       //para numero 4
       else if(letra == '4'){
-      	mostrarleds(cuatro);
+      	mostrarleds(cuatro,tiempo);
       }
       //para numero 5
       else if(letra == '5'){
-      	mostrarleds(cinco);
+      	mostrarleds(cinco,tiempo);
       }
       //para numero 6
       else if(letra == '6'){
-      	mostrarleds(seis);
+      	mostrarleds(seis,tiempo);
       }
       //para numero 7
       else if(letra == '7'){
-      	mostrarleds(siete);
+      	mostrarleds(siete,tiempo);
       }
       //para numero 8
       else if(letra == '8'){
-      	mostrarleds(ocho);
+      	mostrarleds(ocho,tiempo);
       }
       //para numero 9
       else if(letra == '9'){
-      	mostrarleds(nueve);
+      	mostrarleds(nueve,tiempo);
       }
-  
-    
+      //para emoji1
+        else if(letra == '!'){
+          mostrarleds(cara1,tiempo);
+        }
+        //para emoji2
+      else if(letra == '"'){
+      	mostrarleds(cara2,tiempo);
+      }
+    //para emoji3
+        else if(letra == '#'){
+          mostrarleds(cara3,tiempo);
+        }
 }
 void desplazarbyte(uint8_t Pindato, uint8_t Pinreloj, uint8_t val){
   	uint8_t i; 
@@ -288,21 +288,53 @@ void desplazarbyte(uint8_t Pindato, uint8_t Pinreloj, uint8_t val){
             digitalWrite(Pinreloj, LOW);            
       }
   }    
-void llenar_matriz(int **matriz){
 
-  for(int i = 0; i < 36; i++){
-    for(int j = 0; j < 8;j++){
-      //para letra A
-      
-    }  
+void mostrarleds(int L[],int tiempo){
+  int contador = 0;
+  while(true){
+    contador = millis();
+  	for (int i=0; i<=8; i++) {
+    	desplazarbyte(SER,SRCLK,~L[i]);
+    	desplazarbyte(SER,SRCLK,128 >> i); 
+    	digitalWrite(RCLK, 1);
+    	digitalWrite(RCLK, 0);
+  }
+    if(contador > tiempo)
+      break;
   }
 }
-void mostrarleds(int L[]){
-  for (int i=0; i<=8; i++) {
-
-    desplazarbyte(SER,SRCLK,~L[i]);
-    desplazarbyte(SER,SRCLK,128 >> i); 
-    digitalWrite(RCLK, 1);
-    digitalWrite(RCLK, 0);
+void publik(){
+String Lpatron;
+ char charpatron[]="";
+int Npatron, Dpatron;
+  Serial.println("Ingrese la cantidad de patrones que desea visualizar");
+  while(true){
+    if(Serial.available()){
+  	  Npatron = Serial.parseInt();
+      delay(200);
+      break;
+    }
   }
+  Serial.println("Ingrese el tiempo de visualizacion en segundos entre cada patron : ");
+  while(true){
+    if(Serial.available()){
+  	  Dpatron = Serial.parseInt();
+      delay(200);
+      break;     
+    }
+  }
+  Dpatron = Dpatron*1000;
+  Serial.println("Ingrese La secuencia de patrones: ");
+  int largo = 0;
+  while(largo < Npatron){
+    if(Serial.available()){
+      for(int i=0;i<=Npatron;i++){
+        charpatron[i]=Serial.read();
+        imagen(charpatron[i],Dpatron);
+      }
+    }
+  delay(200);
+  }
+ 
+  
 }
